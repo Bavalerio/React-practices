afacdb9dec890fbcf1837c866178325da2238ebd
Bryan Valerio <bryanvale91@gmail.com>
2020-04-22 23:32:29 -0600

    useState and use datePicker for show in to component

--- react-date-picker/src/App.js
+++ react-date-picker/src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
+  const [selectedDate, setSelectedDate] = useState(null);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DatePicker
+        selected={selectedDate}
+        onChange={(date) => setSelectedDate(date)}
+        placeholderText={"dd/mm/yyyy"}
+        // minDate={new Date()}
+        // maxDate={new Date()}
+        filterDate={(date) => date.getDay() !== 6 && date.getDay() !== 0}
+        showYearDropdown
+        scrollableYearDropdown
+      />
     </div>
   );
 }
