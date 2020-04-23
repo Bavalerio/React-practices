752c0de542017a93486512c20674d5e360481d34
Bryan Valerio <bryanvale91@gmail.com>
2020-04-22 21:24:01 -0600

    Create components and install libraries chart- chart-2

--- react-chartjs/src/App.js
+++ react-chartjs/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import LineChart from './components/lineChart'
+import BarChart from './components/barChart'
+import DoughnutChart from './components/doughnutChart'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='chart'>
+        <LineChart />
+        <BarChart />
+        <DoughnutChart />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
