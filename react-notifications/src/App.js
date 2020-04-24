9832cb817fe9decdd74d2f6452667d9a1ab12ec1
Bryan Valerio <bryanvale91@gmail.com>
2020-04-24 17:36:47 -0600

    Custom component use ReactToastify.css

--- react-notifications/src/App.js
+++ react-notifications/src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"
+
+toast.configure();
+
+const CustomToast = ({ closeToast }) => {
+  return (
+    <div>
+      Something went wrong! <button onClick={closeToast}>Close</button>
+    </div>
+  );
+};
 
 function App() {
+  const notify = () => {
+    toast("Default!", { possition: toast.POSITION.TOP_LEFT });
+    toast.success("Success!", {
+      position: toast.POSITION.TOP_CENTER,
+      autoClose: 8000,
+    });
+    toast.info("Info!", {
+      possition: toast.POSITION.TOP_RIGHT,
+      autoClose: false,
+    });
+    toast.warn(<CustomToast />, {
+      possition: toast.POSITION.BOTTOM_LEFT,
+    });
+    toast.error("Error", { possition: toast.POSITION.BOTTOM_CENTER });
+    toast("Wow so easy !", { possition: toast.POSITION.BOTTOM_RIGHT });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={notify}>Notify</button>
     </div>
   );
 }
